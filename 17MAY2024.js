8984d2f67f2b4f9350d711b5153ad6552ffc36e6
Joe Gaston <info@joegaston.com>
2024-05-17 06:19:54 -0700

    17MAY2024 solutions. non complete

---
+++ 17MAY2024.js
@@ -0,0 +1,49 @@
+// ! Codewars Challenge: Fake Binary
+
+//Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+//Note: input will never be an empty string
+
+
+
+
+
+// ! Psuedo code
+
+//split the string into an array using split()
+//next iterate over the array using for each() and an if statement
+//Change the array back to a string using join()
+
+
+
+
+// ! My solution
+
+
+function fakeBin(x){
+let arr = x.split('')
+let newstr = []
+newstr = arr.forEach(element => {
+    if (element > 5) {
+        newstr.push(0)
+    } else {
+        newstr.push(1)
+    }
+    return newstr
+});
+
+}
+
+let str = "123456789"
+
+console.log(fakeBin(str))
+
+
+
+// ! Best practices
+
+
+
+
+
+// !lessons learned
