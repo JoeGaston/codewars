8a2868f4dd85746833fc24e14097d47926df4c03
Joe Gaston <info@joegaston.com>
2024-05-19 20:07:00 -0700

    changes

---
+++ 19MAY2024.js
@@ -0,0 +1,34 @@
+// ! Codewars Challenge: Parse nice int from char problem
+
+// You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+// Write a program that returns the girl's age (0-9) as an integer.
+
+// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+
+// ! Psuedo code
+
+// I need a randon number first. I want to use math.random and math.floor to create this inside the function.
+// Because im not using a prompt, the function will simply take in a string-numer ('9').
+// The first thing i need to do is convert the string to a number. Im going to use the number() method. There is another method called parseint() that i may try as well.
+
+
+
+// ! My solution
+
+function getAge(inputString){
+    // return the girl's correct age as an integer. Happy coding :)
+    }
+
+let ageNum = Math.floor(Math.random() * 10)
+let ageString = ageNum.toString()
+console.log(ageString)
+
+// ! Best practices
+
+
+
+
+
+// !lessons learned
