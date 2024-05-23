075834357c8c77490740931ab0d0fd3275c82fbd
Joe Gaston <info@joegaston.com>
2024-05-23 13:26:01 -0700

    daily solution

--- 23MAY2024.js
+++ 23MAY2024.js
@@ -5,7 +5,7 @@
 // possible price and sell it at the highest.
 
 // Task
-// Write a function that returns both the minimum and maximum number of the given list/array.
+// Write a function that returns both the minimum and maximum number of the given array.
 
 // Examples (Input --> Output)
 // [1,2,3,4,5] --> [1,5]
@@ -20,7 +20,10 @@
 
 // ! Psuedo code
 
-
+// create a function and call that takes in an array
+// Use the Math.max and Math.min method along with the spread (...) operator to access the min/max
+// then push the new min/ max to a new array
+//return the new arr
 
 
 
@@ -28,15 +31,23 @@
 // ! My solution
 
 function minMax(arr){
-    return [0,0]; // fix me!
+  let maxi = Math.max(...arr)
+  let mini = Math.min(...arr)
+  return [mini,maxi]
   }
 
+let arr = [5,3,9,6,11,88,4]
 
+console.log(minMax(arr))
 
 // ! Best practices
 
-
+function minMax(arr){
+  return [Math.min(...arr), Math.max(...arr)];
+}
 
 
 
 // !lessons learned
+
+// I just learned about the spread operator which will allow me to access the individual elements of an array while using the Math.min and Math.max methods.
