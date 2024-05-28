5b895d2ff5ed12d216a1b078098e76c0fba98b50
Joe Gaston <info@joegaston.com>
2024-05-28 05:36:12 -0700

    daily solution

---
+++ 27MAY2024.js
@@ -0,0 +1,34 @@
+// ! Codewars Challenge/ Difficulty: Count of positives / sum of negatives 7 kyu
+
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+
+
+// ! Psuedo code
+
+// I need to be able to iterate through an array. Either use for each of map
+
+
+
+
+// ! My solution
+
+function countPositivesSumNegatives(input) {
+    // your code here
+  }
+
+
+// ! Best practices
+
+
+
+
+
+// !lessons learned
