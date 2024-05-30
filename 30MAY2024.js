e8c20bbcaec15ebf6a417df0b2b2f1fe1a0fcf35
Joe Gaston <info@joegaston.com>
2024-05-30 08:18:24 -0700

    daily solution

---
+++ 30MAY2024.js
@@ -0,0 +1,46 @@
+// ! Codewars Challenge/ Difficulty: Total Amount of Points
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+
+
+
+// ! Psuedo code
+
+// create a function that takes in the object
+// the Object needs to have 10 key value pairs
+
+
+
+
+
+// ! My solution
+function points(games) {
+    return 0
+  }
+
+let scores = ['3:1', '2:2', '1:3', '0:2', '2:0', '3:2', '3:3', '1:1', '1:2', "2:1"]
+
+
+// ! Best practices
+
+
+
+
+
+// !lessons learned
