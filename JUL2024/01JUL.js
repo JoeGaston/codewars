2017b07a2078c0a80e507d9c4cfd72ce1ec8f211
Joe Gaston <info@joegaston.com>
2024-07-01 07:29:59 -0700

    july update

---
+++ JUL2024/01JUL.js
@@ -0,0 +1,44 @@
+// ? Codewars Challenge/ Difficulty: Maskify / 7kyu
+
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Examples (input --> output):
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+// // "What was the name of your first pet?"
+// "Skippy" --> "##ippy"
+// "Nananananananananananananananana Batman!" --> "####################################man!"
+
+
+
+// ! Psuedo code
+// create a function that takes in a string
+// iterate over the string of characters. 
+// need to use the .length method to identify the "last four" characters of a string
+// if [i] is not in the last four digits, use hash
+
+
+
+
+// ! My solution
+
+// return masked string
+function maskify(cc) {
+
+}
+
+
+
+
+// ! Best practices
+
+
+
+
+
+// !lessons learned
