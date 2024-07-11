f7ecc32e01feb840ee77b6f782e1c8a2db06f587
Joe Gaston <info@joegaston.com>
2024-07-11 11:02:50 -0700

    daily solution

--- JUL2024/11JUL.js
+++ JUL2024/11JUL.js
@@ -24,24 +24,107 @@
 
 // ! My solution
 
-function spinWords(string){
-    let newStr = string.split(' ') // This seperates the string into an array of the words so they can be manipulated further.
-    for (let i = 0; i <= newStr.length; i++) { //here is the iteration
-        if (newStr[i].length >= 5) { //this checks for the condition. It appears i cannot use .length after newStr[i]
-            // console.log(newStr[i])
-            newStr[i].split('').reverse().join('') // if condition is met, this will reverse the element
-        } else {
-            continue
-        }
-    }
-  }
+// function spinWords(string){
+//     let newStr = string.split(' ') // This seperates the string into an array of the words so they can be manipulated further.
+//     for (let i = 0; i <= newStr.length; i++) { //here is the iteration
+//         if (newStr[i].length >= 5) { //this checks for the condition. It appears i cannot use .length after newStr[i]
+//             //console.log(newStr[i])
+//             newStr[i] = newStr[i].split('').reverse().join('') // if condition is met, this will reverse the element
+//         }
+//         } 
+//         return newStr.join(' ')
+//     }
 
-spinWords('Hey fellow warriors')
+// console.log(spinWords('Hey fellow warriors'))
 
 // ! Best practices
 
+// function spinWords(str) {
+//     let strArr = str.split(' ');
+//     for (let i = 0; i < strArr.length; i++) {
+//       if (strArr[i].length >= 5) {
+//         strArr[i] = strArr[i].split('').reverse().join('');
+//       }
+//     }
+//     return strArr.join(' ');
+//   }
+
+// console.log(spinWords('Hey fellow warriors'))
 
 
 
+// !lessons learned
+
+
+
+// ? Codewars Challenge/ Difficulty: duplicate encoder / 6kyu
+
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+// Notes
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+
+
+// ! Psuedo code
+// Create a function that takes in a string <>
+// Define an empty new string <>
+// Need to iterate through the string characters <>
+    // need to be able to analyze how many times a chracter appears? Use a counter? Nested loop?
+    // if character appears once newStr.push('(')
+    // if character appears more than once newStr.push(')')
+// return newStr
+
+
+
+
+
+// ! My solution
+// function duplicateEncode(word){
+//     let newStr = ''
+//     // this is the outer loop
+//     for (let i = 0; i <= word.length; i++) {
+//         // this is the inner loop
+//         for (let j = 0; j <= word.length; i++) {
+//             let counter = -1 // need a counter given current process as a measurable indicator of letter repitition.
+//             if (word[i] === word[j]) { // this compares the letters.
+//                 counter += 1
+//             } else {
+//                 counter + 0
+//             }
+//             return counter
+//         break
+//         } 
+//     }
+// }
+
+// console.log(duplicateEncode('recede'))
+
+// ! Best practices
+
+function duplicateEncode(word){
+   
+    var unique='';
+    word = word.toLowerCase();
+    for(var i=0; i<word.length; i++){
+        if(word.lastIndexOf(word[i]) == word.indexOf(word[i])){ // if the last time it appears is equal to the first time it appears...
+            console.log(word.lastIndexOf(word[i]))
+            console.log(word.indexOf(word[i]))
+            unique += '(';
+        }
+        else{
+            unique += ')';
+        }
+    }
+    return unique;
+
+}
+
+console.log(duplicateEncode('recede'))
 
 // !lessons learned
